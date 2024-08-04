58279bca103cbdc9cf4a65deae1a5a16b71282f8
Petersix <150849737+peterarcuri@users.noreply.github.com>
2024-08-03 23:51:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+// (Letter Count Psueudocode)
+// create a function called letterOccurrence that takes in an argument(word)
+// create variable for alphabetical characters
+
+// write a conditional printing out "ERROR" if argument(word) is not a string (typeof word !== "string"
+
+// Create a dictionary (object) to hold letter count
+
+// create a for loop for each letter in (word) 
+// need a variable for each letter to lowercase
+
+// if the dictionary has a letter:
+
+// incremenet by 1 (++)
+
+// else
+
+// set the letterCount to 1
+
+// return letterCount
 
 function letterOccurrence(word) {
 
